24ae9dd26c1eca53e2cb63b388c93a42a31b0618
Balakrishna M <balakrishna.moghri@infra.market>
2024-08-09 13:09:13 +0530

    Adding breadcrumb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-// File: app/layout.tsx
-
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { AuthProvider } from './contexts/AuthContext'
+import Breadcrumb from './components/Breadcrumb';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,7 +23,12 @@ export default function RootLayout({
       <body className={`${inter.className} min-h-screen flex flex-col bg-gray-50`}>
         <AuthProvider>
           <Header />
-          <main className="flex-grow">{children}</main>
+          <main className="flex-grow pt-2">
+            <Breadcrumb />
+            <div className="container mx-auto px-4 py-8 bg-white rounded-lg shadow-md bg-opacity-90">
+              {children}
+            </div>
+          </main>
           <Footer />
         </AuthProvider>
       </body>
